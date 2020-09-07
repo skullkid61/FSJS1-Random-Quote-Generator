7b33c6e123adc4e38422d30e5f97068fd71adacb
skullkid61 <43558378+skullkid61@users.noreply.github.com>
2020-09-07 21:55:45 +0930

    Finished project

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ const quotes = [
     source: 'Andy Dufresne',
     citation: 'The Shawshank Redemption',
     year: 1994,
-    tags: ['movie', 'prison']
+    tags: ['Movie', 'Prison']
   },
   {
     quote: 'The first step toward success is taken when you refuse to be a captive of the environment in which you first find yourself',
@@ -26,6 +26,7 @@ const quotes = [
   {
     quote: 'When one door of happiness closes, another opens; but often we look so long at the closed door that we do not see the one which has been opened for us.',
     source: 'Helen Keller',
+    year: 1962
   },
   {
     quote: 'Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do.',
@@ -42,24 +43,52 @@ const quotes = [
 /***
  * `getRandomQuote` function
 ***/
+// This makes sure that the same number doesn't come up twice
+let randomNumber;
+let savedNumber;
 // This will generate a random index number for the quote and return
-function getRandomQuote () {
-  const random = Math.floor(Math.random() * quotes.length);
-  return quotes[random];
+const getRandomQuote = () => {
+    do {
+    randomNumber = Math.floor(Math.random() * quotes.length); 
+  } while (randomNumber === savedNumber)
+  savedNumber = randomNumber;
+  return quotes[randomNumber];
 }
 
-
+/***
+ * `randomRGB` function
+***/
+const randomRGB = () => {
+  const randomColor = () => {
+    return Math.floor(Math.random() * 255);
+  }
+  document.body.style.backgroundColor = `rgb(${randomColor()},${randomColor()},${randomColor()})`
+}
 
 /***
  * `printQuote` function
 ***/
-function printQuote () {
-  const quote = getRandomQuote();
-  const quoteBox = document.querySelector('#quote-box');
-  quoteBox.innerHTML = `<p class="quote">${quote.quote}</p>
-  <p class="source">${quote.source}<span class="citation">${quote.citation}</span><span class="year">${quote.year}</span></p>`
+const printQuote = () => {
+  const randomQuote = getRandomQuote();
+  let quoteHTML = `<p class="quote">${randomQuote.quote}</p>
+    <p class="source">${randomQuote.source}`
+  if (randomQuote.citation) {
+    quoteHTML += `<span class="citation">${randomQuote.citation}</span>`
+  }
+  if (randomQuote.year) {
+    quoteHTML += `<span class="year">${randomQuote.year}</span>`
+  }
+  if (randomQuote.tags) {
+    quoteHTML += ` <span>Tags: ${randomQuote.tags.join(', ')}</span>`
+  }
+  quoteHTML += `</p>`
+  document.querySelector('#quote-box').innerHTML = quoteHTML;
+  randomRGB();
 }
 
+
+// Automatically printQuote every 10 seconds
+setInterval(printQuote, 10000);
 
 /***
  * click event listener for the print quote button
